98e227bf148e433f09301f3fef733fdc61f25de6
Aidan Holloway-bidwell <aidanhb@google.com>
2019-05-14 16:05:37 -0700

    Add extra connection failsafe in A2dpCodecBaseTest.py
    Headset and phone occasionally fail to connect in setup_test. Upon one
    failure, reset bluetooth on phone and try again.
    
    Bug: None
    Test: Tested on Pixel3/Android Q and Earstudio relay device
    Change-Id: I227fa77013209999a07aa3be8757ea0856bae911

--- acts/framework/acts/test_utils/bt/A2dpCodecBaseTest.py
+++ acts/framework/acts/test_utils/bt/A2dpCodecBaseTest.py
@@ -82,8 +82,11 @@ class A2dpCodecBaseTest(BluetoothBaseTest):
                         'thdn': []}
 
         self.log.info('Pairing and connecting to headset...')
+        if not connect_phone_to_headset(self.android, self.bt_device, 20):
+            self.log.info("Failed to connect. Factory reset Bluetooth.")
+            self.android.droid.bluetoothFactoryReset()
         asserts.assert_true(
-            connect_phone_to_headset(self.android, self.bt_device, 600),
+            connect_phone_to_headset(self.android, self.bt_device, 60),
             'Could not connect to device at address %s'
             % self.bt_device.mac_address,
             extras=self.metrics)
