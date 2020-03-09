9ef4b142512e06f05d2ff60dec7a3ac3edf5c6f6
Hector <htellez@google.com>
2020-03-09 11:49:01 -0700

    Adding new metrics logger for bounded metrics.
    Bug: 143720953
    Test: Unit tests added
    Test: Executed PowerPresubmitTest.test_quick_idle locallly
    Change-Id: I582abb647a5240c700def94bfb7fb2c2bcd6d24f

--- acts/framework/acts/test_utils/instrumentation/power/power_metrics.py
+++ acts/framework/acts/test_utils/instrumentation/power/power_metrics.py
@@ -16,10 +16,8 @@
 
 import math
 
-from acts.test_utils.instrumentation import instrumentation_proto_parser \
-    as parser
-from acts.test_utils.instrumentation.instrumentation_base_test \
-    import InstrumentationTestError
+from acts.test_utils.instrumentation import instrumentation_proto_parser as parser
+from acts.test_utils.instrumentation.instrumentation_base_test import InstrumentationTestError
 
 # Unit type constants
 CURRENT = 'current'
@@ -55,6 +53,129 @@ CONVERSION_TABLES = {
 }
 
 
+class AbsoluteThresholds(object):
+    """Class to represent thresholds in absolute (non-relative) values.
+
+    Attributes:
+        lower: Lower limit of the threshold represented by a measurement.
+        upper: Upper limit of the threshold represented by a measurement.
+        unit_type: Type of the unit (current, power, etc).
+        unit: The  unit for this threshold (W, mW, uW).
+    """
+
+    def __init__(self, lower, upper, unit_type, unit):
+        self.unit_type = unit_type
+        self.unit = unit
+        self.lower = Measurement(lower, unit_type, unit)
+        self.upper = Measurement(upper, unit_type, unit)
+
+    @staticmethod
+    def from_percentual_deviation(expected, percentage, unit_type, unit):
+        """Creates an AbsoluteThresholds object from an expected value and its
+        allowed percentual deviation (also in terms of the expected value).
+
+        For example, if the expected value is 20 and the deviation 25%, this
+        would imply that the absolute deviation is 20 * 0.25 = 5 and therefore
+        the absolute threshold would be from (20-5, 20+5) or (15, 25).
+
+        Args:
+            expected: Central value from which the deviation will be estimated.
+            percentage: Percentage of allowed deviation, the percentage itself
+            is in terms of the expected value.
+            unit_type: Type of the unit (current, power, etc).
+            unit: Unit for this threshold (W, mW, uW).
+        """
+        return AbsoluteThresholds(expected * (1 - percentage / 100),
+                                  expected * (1 + percentage / 100),
+                                  unit_type,
+                                  unit)
+
+    @staticmethod
+    def from_threshold_conf(thresholds_conf):
+        """Creates a AbsoluteThresholds object from a ConfigWrapper describing
+        a threshold (either absolute or percentual).
+
+        Args:
+            thresholds_conf: ConfigWrapper object that describes a threshold.
+        Returns:
+            AbsolutesThresholds object.
+        Raises:
+            ValueError if configuration is incorrect or incomplete.
+        """
+        if 'unit_type' not in thresholds_conf:
+            raise ValueError(
+                'A threshold config must contain a unit_type. %s is incorrect'
+                % str(thresholds_conf))
+
+        if 'unit' not in thresholds_conf:
+            raise ValueError(
+                'A threshold config must contain a unit. %s is incorrect'
+                % str(thresholds_conf))
+
+        unit_type = thresholds_conf['unit_type']
+        unit = thresholds_conf['unit']
+
+        is_relative = (
+            'expected_value' in thresholds_conf and
+            'percent_deviation' in thresholds_conf)
+
+        is_almost_relative = (
+            'expected_value' in thresholds_conf or
+            'percent_deviation' in thresholds_conf)
+
+        is_absolute = ('lower_limit' in thresholds_conf or
+                       'upper_limit' in thresholds_conf)
+
+        if is_absolute and is_almost_relative:
+            raise ValueError(
+                'Thresholds can either be absolute (with lower_limit and'
+                'upper_limit defined) or by percentual deviation (with'
+                'expected_value and percent_deviation defined), but never'
+                'a mixture of both. %s is incorrect'
+                % str(thresholds_conf))
+
+        if is_almost_relative and not is_relative:
+            if 'expected_value' not in thresholds_conf:
+                raise ValueError(
+                    'Incomplete definition of a threshold by percentual '
+                    'deviation. percent_deviation given, but missing '
+                    'expected_value. %s is incorrect'
+                    % str(thresholds_conf))
+
+            if 'percent_deviation' not in thresholds_conf:
+                raise ValueError(
+                    'Incomplete definition of a threshold by percentual '
+                    'deviation. expected_value given, but missing '
+                    'percent_deviation. %s is incorrect'
+                    % str(thresholds_conf))
+
+        if not is_absolute and not is_relative:
+            raise ValueError(
+                'Thresholds must be either absolute (with lower_limit and'
+                'upper_limit defined) or defined by percentual deviation (with'
+                'expected_value and percent_deviation defined). %s is incorrect'
+                % str(thresholds_conf))
+
+        if is_relative:
+            expected = thresholds_conf.get_numeric('expected_value')
+            percent = thresholds_conf.get_numeric('percent_deviation')
+
+            thresholds = (
+                AbsoluteThresholds.from_percentual_deviation(
+                    expected,
+                    percent,
+                    unit_type, unit))
+
+        else:
+            lower_value = thresholds_conf.get_numeric('lower_limit',
+                                                      float('-inf'))
+            upper_value = thresholds_conf.get_numeric('upper_limit',
+                                                      float('inf'))
+            thresholds = AbsoluteThresholds(lower_value, upper_value, unit_type,
+                                            unit)
+        return thresholds
+
+
 class Measurement(object):
     """Base class for describing power measurement values. Each object contains
     an value and a unit. Enables some basic arithmetic operations with other
@@ -68,7 +189,9 @@ class Measurement(object):
 
     def __init__(self, value, unit_type, unit):
         if unit_type not in CONVERSION_TABLES:
-            raise TypeError('%s is not a valid unit type' % unit_type)
+            raise TypeError(
+                '%s is not a valid unit type, valid unit types are %s' % (
+                    unit_type, str(CONVERSION_TABLES.keys)))
         self._value = value
         self._unit_type = unit_type
         self._unit = unit
@@ -224,16 +347,16 @@ class PowerMetrics(object):
                 self._voltage, self._start_time)
             try:
                 test_starts[test_name] = Measurement(
-                    times[parser.START_TIMESTAMP], TIME, MILLISECOND) \
-                                             .to_unit(SECOND).value - self._start_time
+                    times[parser.START_TIMESTAMP], TIME, MILLISECOND).to_unit(
+                    SECOND).value - self._start_time
             except KeyError:
                 raise InstrumentationTestError(
                     'Missing start timestamp for test scenario "%s". Refer to '
                     'instrumentation_proto.txt for details.' % test_name)
             try:
                 test_ends[test_name] = Measurement(
-                    times[parser.END_TIMESTAMP], TIME, MILLISECOND) \
-                                           .to_unit(SECOND).value - self._start_time
+                    times[parser.END_TIMESTAMP], TIME, MILLISECOND).to_unit(
+                    SECOND).value - self._start_time
             except KeyError:
                 raise InstrumentationTestError(
                     'Missing end timestamp for test scenario "%s". Test '
