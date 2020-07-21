fe10c670579c9c5a05f26d931db319d1757a4dbb
Nate Jiang <qiangjiang@google.com>
2020-07-21 11:07:44 -0700

    [ACTS] Fix subscribe config ranging enabled
    Subscribe config should use set min/max to enable ranging.
    
    Bug: 159163836
    Test: acts RangeAwareTest
    Change-Id: I70b9300547ab80ee6271b6e19b8fddacf50db7e2

--- acts/tests/google/wifi/rtt/functional/RangeAwareTest.py
+++ acts/tests/google/wifi/rtt/functional/RangeAwareTest.py
@@ -122,9 +122,10 @@ class RangeAwareTest(AwareBaseTest, RttBaseTest):
                  autils.create_discovery_config(
                      self.SERVICE_NAME, aconsts.PUBLISH_TYPE_UNSOLICITED),
                  True),
-             s_config=autils.add_ranging_to_pub(
+             s_config=autils.add_ranging_to_sub(
                  autils.create_discovery_config(
-                     self.SERVICE_NAME, aconsts.SUBSCRIBE_TYPE_PASSIVE), True),
+                     self.SERVICE_NAME, aconsts.SUBSCRIBE_TYPE_PASSIVE),
+                 self.DISTANCE_MIN, self.DISTANCE_MAX),
              device_startup_offset=self.device_startup_offset,
              msg_id=self.get_next_msg_id())
 
@@ -302,8 +303,8 @@ class RangeAwareTest(AwareBaseTest, RttBaseTest):
             disable_publish: if true disable ranging on publish config, otherwise disable ranging on
                             subscribe config
         """
-        p_dut = self.android_devices[1]
-        s_dut = self.android_devices[0]
+        p_dut = self.android_devices[0]
+        s_dut = self.android_devices[1]
         pub_config = autils.create_discovery_config(
             self.SERVICE_NAME, aconsts.PUBLISH_TYPE_UNSOLICITED)
         sub_config = autils.create_discovery_config(
