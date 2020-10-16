31fcb819e03e08db6291e2411274e6f0cf3763c8
James Kacherla <jkacherla@google.com>
2020-10-16 14:32:56 -0700

    Adding annotations and descriptions to commands.
    BYPASS_INCLUSIVE_LANGUAGE_REASON=hardcoded expected value that can be changed in this cl.
    
    Bug: 169925189
    Test: N/A
    Change-Id: Iad9f2a0a395836069917984ad84668900041acb8

--- acts/framework/acts/test_utils/instrumentation/device/command/adb_commands/goog.py
+++ acts/framework/acts/test_utils/instrumentation/device/command/adb_commands/goog.py
@@ -13,6 +13,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+"""Google-internal device settings for power testing."""
 
 from acts.test_utils.instrumentation.device.command.adb_command_types import \
   DeviceBinaryCommandSeries
@@ -27,78 +28,85 @@ from acts.test_utils.instrumentation.device.command.adb_command_types import \
 from acts.test_utils.instrumentation.device.command.adb_command_types import \
   GenericCommand
 
-"""Google-internal device settings for power testing."""
-
-# TODO: add descriptions to each setting
-
 # Location
 
 location_collection = DeviceGServices(
-    'location:collection_enabled', on_val='1', off_val='0',
+    'location:collection_enabled',
+    on_val='1',
+    off_val='0',
     desc='Modifies whether collecting location is enabled.')
 
 location_off_warning_dialog = DeviceGServices(
-    'location:enable_location_off_warning_dialog', on_val='true', off_val='false',
+    'location:enable_location_off_warning_dialog', on_val='true',
+    off_val='false',
     desc='Modifies whether the location off warning dialog should appear.'
 )
 
-location_opt_in = DeviceBinaryCommandSeries(
-    [
-        DeviceState('content insert --uri content://com.google.settings/'
-                    'partner --bind name:s:use_location_for_services '
-                    '--bind value:s:%s',
-                    desc='Modifies whether using location for services is '
-                         'allowed.'),
-        DeviceState('content insert --uri content://com.google.settings/'
-                    'partner --bind name:s:network_location_opt_in '
-                    '--bind value:s:%s',
-                    desc='Modifies whether to allow location over network.')
-    ]
-)
+location_opt_in = DeviceBinaryCommandSeries([
+    DeviceState(
+        'content insert --uri content://com.google.settings/'
+        'partner --bind name:s:use_location_for_services '
+        '--bind value:s:%s',
+        desc='Modifies whether using location for services is '
+             'allowed.'),
+    DeviceState(
+        'content insert --uri content://com.google.settings/'
+        'partner --bind name:s:network_location_opt_in '
+        '--bind value:s:%s',
+        desc='Modifies whether to allow location over network.')
+])
 
 location_activity_low_power = DeviceGServices(
-    'location:activity_low_power_mode_enabled', on_val='true', off_val='false',
+    'location:activity_low_power_mode_enabled',
+    on_val='true',
+    off_val='false',
     desc='Enables or disables location activity low power mode.'
 )
 
 location_policy_set = GenericCommand(
-  'am broadcast -a com.google.gservices.intent.action.GSERVICES_OVERRIDE '
-  '-e gms:chimera:dev_module_packages "com.google.android.gms.policy_location"',
-  desc='Allows location policy set'
+    'am broadcast -a com.google.gservices.intent.action.GSERVICES_OVERRIDE '
+    '-e gms:chimera:dev_module_packages '
+    '"com.google.android.gms.policy_location"',
+    desc='Allows location policy set'
 )
 
 # Cast
 cast_broadcast = DeviceGServices(
     'gms:cast:mdns_device_scanner:is_enabled',
-    desc='Disable devices sending CAST messages to prevent frequent wakeups by WiFi')
+    desc='Disable devices sending CAST messages to prevent frequent wakeups '
+         'by WiFi')
 
 # Apps
-disable_chrome = GenericCommand('pm disable-user com.android.chrome',
-                                desc='Disables the Google chrome.')
+disable_chrome = GenericCommand(
+    'pm disable-user com.android.chrome', desc='Disables the Google chrome.')
 
-force_stop_nexuslauncher = GenericCommand('am force-stop com.google.android.apps.nexuslauncher',
-                                          desc='Force stop nexus launcher.')
+force_stop_nexuslauncher = GenericCommand(
+    'am force-stop com.google.android.apps.nexuslauncher',
+    desc='Force stop nexus launcher.')
 
-disable_playstore = GenericCommand('pm disable-user com.android.vending',
-                                   desc='Disables the Google playstore.')
+disable_playstore = GenericCommand(
+    'pm disable-user com.android.vending',
+    desc='Disables the Google playstore.')
 
 duo_grant_camera = GenericCommand(
     'pm grant com.google.android.apps.faketachyon android.permission.CAMERA',
     desc='Grants camera permissions to the duo(faketachyon) app')
 
 duo_grant_audio = GenericCommand(
-    'pm grant com.google.android.apps.faketachyon android.permission.RECORD_AUDIO',
+    'pm grant '
+    'com.google.android.apps.faketachyon android.permission.RECORD_AUDIO',
     desc='Grants audio permissions to the duo(faketachyon) app')
 
 duo_grant_contacts = GenericCommand(
-    'pm grant com.google.android.apps.faketachyon android.permission.READ_CONTACTS',
+    'pm grant '
+    'com.google.android.apps.faketachyon android.permission.READ_CONTACTS',
     desc='Grants contacts permissions to the duo(faketachyon) app')
-
 
 # Volta
 
-disable_volta = GenericCommand('pm disable-user com.google.android.volta',
-                               desc='Disables the volta app.')
+disable_volta = GenericCommand(
+    'pm disable-user com.google.android.volta',
+    desc='Disables the volta app.')
 
 # Betterbug
 
@@ -108,11 +116,12 @@ disable_betterbug = GenericCommand(
 
 # CHRE
 
-disable_chre = GenericCommand('setprop ctl.stop vendor.chre',
-                              desc='Disables chre.')
+disable_chre = GenericCommand(
+    'setprop ctl.stop vendor.chre',
+    desc='Disables chre.')
 
 enable_chre = GenericCommand('setprop ctl.start vendor.chre',
-                              desc='Enables chre.')
+                             desc='Enables chre.')
 
 # MusicIQ
 
@@ -125,7 +134,6 @@ enable_musiciq = GenericCommand(
     'am start -n com.google.intelligence.sense/com.google.intelligence.sense.'
     'ambientmusic.AmbientMusicSettingsActivity',
     desc='Enables the musiciq feature')
-
 
 # Email
 
@@ -150,41 +158,48 @@ camera_hdr_mode = DeviceSetprop(
     'camera.optbar.hdr', on_val='true', off_val='false',
     desc="Modifies whether to use HDR camera mode.")
 
+# persists across reboots
 compact_location_log = DeviceGServices(
     'location:compact_log_enabled',
-    desc = 'Enables compact location logging')
+    desc='Enables compact location logging')
 
+# persists across reboots
 magic_tether = DeviceGServices('gms:magictether:enable',
                                desc='Disables magic tethering')
 
+# persists across reboots
 ocr = DeviceGServices('ocr.cc_ocr_enabled',
-                      desc = 'Turns off OCR download for testing purposes')
+                      desc='Turns off OCR download for testing purposes')
 
+# persists across reboots
 phenotype = DeviceGServices(
     'gms:phenotype:phenotype_flag:debug_bypass_phenotype',
-    desc = 'Bypasses GMS core phenotype experiments')
+    desc='Bypasses GMS core phenotype experiments')
 
+# persists across reboots
 icing = DeviceGServices('gms_icing_extension_download_enabled',
-                        desc = 'Turns off icing download for testing purposes')
+                        desc='Turns off icing download for testing purposes')
 
 edge_sensor = DeviceSetting(
     DeviceSetting.SECURE, 'assist_gesture_enabled',
     desc='Modifies whether the edge sensor gesture is enabled.')
 
 edge_sensor_wakeup = DeviceSetting(
-    DeviceSetting.SECURE, 'assist_gesture_wake_enabled',
+    DeviceSetting.SECURE,
+    'assist_gesture_wake_enabled',
     desc='Modifies whether the edge sensor gesture is allowed to wake up '
          'the device.')
 
 edge_sensor_alerts = DeviceSetting(
-    DeviceSetting.SECURE, 'assist_gesture_silence_alerts_enabled',
+    DeviceSetting.SECURE,
+    'assist_gesture_silence_alerts_enabled',
     desc='Triggers activation/deactivation of edge sensor gesture. It '
          'depends on the settings assist_gesture_enabled and '
          'assist_gesture_wake_enabled to be previously set.')
 
-activity_recognition = GenericCommand('am instrument -e interval_ms 180000'
-                                      ' -e class com.google.android.gms.testapps.location.activity.power.'
-                                      'ActivityPowerUtil#testRegisterAR -w '
-                                      'com.google.android.gms.testapps.location.'
-                                      'activity.power/.ARTestRunner',
-                                      desc = 'Turns activity recognition on')
+activity_recognition = GenericCommand(
+    'am instrument -e interval_ms 180000'
+    ' -e class com.google.android.gms.testapps.location.activity.power.'
+    'ActivityPowerUtil#testRegisterAR -w '
+    'com.google.android.gms.testapps.location.activity.power/.ARTestRunner',
+    desc='Turns activity recognition on')
