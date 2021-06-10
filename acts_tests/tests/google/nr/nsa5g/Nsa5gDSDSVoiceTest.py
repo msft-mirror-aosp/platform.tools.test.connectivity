645884a22295de440911fc8b93a08a1015394aa4
Markus Liu <markusliu@google.com>
2021-06-10 19:19:11 +0800

    Added new test cases into the test class Nsa5gDSDSVoiceTest
    16 new test cases in which one 5G slot is non-DDS slot are added into Nsa5gDSDSVoiceTest.
    
    - test_msim_voice_psim_mo_5g_nsa_volte_esim_5g_nsa_volte_dds_0
    - test_msim_voice_psim_mt_5g_nsa_volte_esim_5g_nsa_volte_dds_0
    - test_msim_voice_esim_mo_5g_nsa_volte_psim_5g_nsa_volte_dds_0
    - test_msim_voice_esim_mt_5g_nsa_volte_psim_5g_nsa_volte_dds_0
    - test_msim_voice_psim_mo_5g_nsa_volte_esim_5g_nsa_volte_dds_1
    - test_msim_voice_psim_mt_5g_nsa_volte_esim_5g_nsa_volte_dds_1
    - test_msim_voice_esim_mo_5g_nsa_volte_psim_5g_nsa_volte_dds_1
    - test_msim_voice_esim_mt_5g_nsa_volte_psim_5g_nsa_volte_dds_1
    - test_msim_voice_psim_mo_5g_nsa_volte_esim_4g_volte_dds_1
    - test_msim_voice_psim_mt_5g_nsa_volte_esim_4g_volte_dds_1
    - test_msim_voice_esim_mo_4g_volte_psim_5g_nsa_volte_dds_1
    - test_msim_voice_esim_mt_4g_volte_psim_5g_nsa_volte_dds_1
    - test_msim_voice_psim_mo_4g_volte_esim_5g_nsa_volte_dds_0
    - test_msim_voice_psim_mt_4g_volte_esim_5g_nsa_volte_dds_0
    - test_msim_voice_esim_mo_5g_nsa_volte_psim_4g_volte_dds_0
    - test_msim_voice_esim_mt_5g_nsa_volte_psim_4g_volte_dds_0
    
    Bug: None
    Test: Yes, locally
    Change-Id: I673db5398499144b3a61ffefeeb496e9c9eaaa0e

--- acts_tests/tests/google/nr/nsa5g/Nsa5gDSDSVoiceTest.py
+++ acts_tests/tests/google/nr/nsa5g/Nsa5gDSDSVoiceTest.py
@@ -26,7 +26,145 @@ class Nsa5gDSDSVoiceTest(TelephonyBaseTest):
     def teardown_test(self):
         ensure_phones_idle(self.log, self.android_devices)
 
-    # psim 5g nsa volte & esim 4g volte
+    # psim 5g nsa volte & esim 5g nsa volte & dds slot 0
+    @test_tracker_info(uuid="8a8c3f42-f5d7-4299-8d84-64ac5377788f")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mo_5g_nsa_volte_esim_5g_nsa_volte_dds_0(self):
+        """A MO VoLTE call dialed at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            0,
+            None,
+            0,
+            mo_rat=["5g_volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="b05b6aea-7c48-4412-b0b1-f57192fc786c")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mt_5g_nsa_volte_esim_5g_nsa_volte_dds_0(self):
+        """A MT VoLTE call received at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            0,
+            0,
+            mt_rat=["5g_volte", "5g_volte"],
+            call_direction="mt")
+
+    @test_tracker_info(uuid="213d5e6f-97df-4c2a-9745-4e40a704853a")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mo_5g_nsa_volte_psim_5g_nsa_volte_dds_0(self):
+        """A MO VoLTE call dialed at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            1,
+            None,
+            0,
+            mo_rat=["5g_volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="48a06a2f-b3d0-4b0e-85e5-2d439ee3147b")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mt_5g_nsa_volte_psim_5g_nsa_volte_dds_0(self):
+        """A MT VoLTE call received at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            1,
+            0,
+            mt_rat=["5g_volte", "5g_volte"],
+            call_direction="mt")
+
+    # psim 5g nsa volte & esim 5g nsa volte & dds slot 1
+    @test_tracker_info(uuid="406bd5e5-b549-470d-b15a-20b4bb5ff3db")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mo_5g_nsa_volte_esim_5g_nsa_volte_dds_1(self):
+        """A MO VoLTE call dialed at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 1)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            0,
+            None,
+            1,
+            mo_rat=["5g_volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="a1e52cee-78ab-4d6e-859b-faf542b8056b")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mt_5g_nsa_volte_esim_5g_nsa_volte_dds_1(self):
+        """A MT VoLTE call received at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 1)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            0,
+            1,
+            mt_rat=["5g_volte", "5g_volte"],
+            call_direction="mt")
+
+    @test_tracker_info(uuid="3b9d796c-b658-4bff-aae0-1243ce8c3d54")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mo_5g_nsa_volte_psim_5g_nsa_volte_dds_1(self):
+        """A MO VoLTE call dialed at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 1)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            1,
+            None,
+            1,
+            mo_rat=["5g_volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="e3edd065-72e1-4067-901c-1454706e9f43")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mt_5g_nsa_volte_psim_5g_nsa_volte_dds_1(self):
+        """A MT VoLTE call received at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at pSIM (slot 1)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            1,
+            1,
+            mt_rat=["5g_volte", "5g_volte"],
+            call_direction="mt")
+
+    # psim 5g nsa volte & esim 4g volte & dds slot 0
     @test_tracker_info(uuid="2890827d-deb2-42ea-921d-3b45f7645d61")
     @TelephonyBaseTest.tel_test_wrap
     def test_msim_voice_psim_mo_5g_nsa_volte_esim_4g_volte_dds_0(self):
@@ -95,7 +233,145 @@ class Nsa5gDSDSVoiceTest(TelephonyBaseTest):
             mt_rat=["5g_volte", "volte"],
             call_direction="mt")
 
-    # psim 4g volte & esim 5g nsa volte
+    # psim 5g nsa volte & esim 4g volte & dds slot 1
+    @test_tracker_info(uuid="9dfa66cc-f464-4964-9e5a-07e01d3e263e")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mo_5g_nsa_volte_esim_4g_volte_dds_1(self):
+        """A MO VoLTE call dialed at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 4G VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            0,
+            None,
+            1,
+            mo_rat=["5g_volte", "volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="97e9ecc0-e377-46a8-9b13-ecedcb98922b")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mt_5g_nsa_volte_esim_4g_volte_dds_1(self):
+        """A MT VoLTE call received at pSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 4G VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            0,
+            1,
+            mt_rat=["5g_volte", "volte"],
+            call_direction="mt")
+
+    @test_tracker_info(uuid="5814cd18-e33b-45c5-b129-bec7e3992d8e")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mo_4g_volte_psim_5g_nsa_volte_dds_1(self):
+        """A MO VoLTE call dialed at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 4G VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            1,
+            None,
+            1,
+            mo_rat=["5g_volte", "volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="457dd160-f7b1-4cfd-920f-1f5ab64f6d78")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mt_4g_volte_psim_5g_nsa_volte_dds_1(self):
+        """A MT VoLTE call received at eSIM, where
+            - pSIM 5G NSA VoLTE
+            - eSIM 4G VoLTE
+            - DDS at pSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            1,
+            1,
+            mt_rat=["5g_volte", "volte"],
+            call_direction="mt")
+
+    # psim 4g volte & esim 5g nsa volte & dds slot 0
+    @test_tracker_info(uuid="db5fca13-bcd8-420b-9953-256186efa290")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mo_4g_volte_esim_5g_nsa_volte_dds_0(self):
+        """A MO VoLTE call dialed at pSIM, where
+            - pSIM 4G VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at eSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            0,
+            None,
+            0,
+            mo_rat=["volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="2fe76eda-20b2-46ab-a1f4-c2c2bc501f38")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_psim_mt_4g_volte_esim_5g_nsa_volte_dds_0(self):
+        """A MT VoLTE call received at pSIM, where
+            - pSIM 4G VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at eSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            0,
+            0,
+            mt_rat=["volte", "5g_volte"],
+            call_direction="mt")
+
+    @test_tracker_info(uuid="90005074-e21f-47c3-9965-54b513214600")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mo_5g_nsa_volte_psim_4g_volte_dds_0(self):
+        """A MO VoLTE call dialed at eSIM, where
+            - pSIM 4G VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at eSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            1,
+            None,
+            0,
+            mo_rat=["volte", "5g_volte"],
+            call_direction="mo")
+
+    @test_tracker_info(uuid="eaf94a45-66d0-41d0-8cb2-153fa3f751f9")
+    @TelephonyBaseTest.tel_test_wrap
+    def test_msim_voice_esim_mt_5g_nsa_volte_psim_4g_volte_dds_0(self):
+        """A MT VoLTE call received at eSIM, where
+            - pSIM 4G VoLTE
+            - eSIM 5G NSA VoLTE
+            - DDS at eSIM (slot 0)
+        """
+        return dsds_voice_call_test(
+            self.log,
+            self.android_devices,
+            None,
+            1,
+            0,
+            mt_rat=["volte", "5g_volte"],
+            call_direction="mt")
+
+    # psim 4g volte & esim 5g nsa volte & dds slot 1
     @test_tracker_info(uuid="8ee47ad7-24b6-4cd3-9443-6ab677695eb7")
     @TelephonyBaseTest.tel_test_wrap
     def test_msim_voice_psim_mo_4g_volte_esim_5g_nsa_volte_dds_1(self):
