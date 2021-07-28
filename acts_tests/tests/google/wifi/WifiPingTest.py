dc6d885369c472ead41cc8f1987a4b9ec5c7bf7a
Xianyuan Jia <xianyuanjia@google.com>
2021-07-28 17:12:33 -0400

    Cherry-pick "Add retry feature to attenuators and improve WiFi test robustness." to AOSP
    This CL implements a retry feature to telnet-based minicircuits
    attenuators. Previously, the telnet helper class would diagnose the
    telnet connection upon command failure and then raise an exception
    regardless of diagnosis. This CL optionally retries a failed telnet
    command once if the diagnose_telnet function indicates that the
    connection is likely recovered. This CL also makes minor modifications
    to WiFi tests to take advantage of this retry, and otherwise improve
    robustness.
    
    Test: Done locally.
    Bug: 186892365
    
    Signed-off-by: Omar El Ayach <oelayach@google.com>
    Change-Id: I11415c8f75f6c14633fbb4d36202c92eafb3b6ba
    Merged-In: I11415c8f75f6c14633fbb4d36202c92eafb3b6ba

--- acts_tests/tests/google/wifi/WifiPingTest.py
+++ acts_tests/tests/google/wifi/WifiPingTest.py
@@ -308,7 +308,7 @@ class WifiPingTest(base_test.BaseTestClass):
         zero_counter = 0
         for atten in testcase_params['atten_range']:
             for attenuator in self.attenuators:
-                attenuator.set_atten(atten, strict=False)
+                attenuator.set_atten(atten, strict=False, retry=True)
             rssi_future = wputils.get_connected_rssi_nb(
                 self.dut,
                 int(testcase_params['ping_duration'] / 2 /
@@ -327,8 +327,8 @@ class WifiPingTest(base_test.BaseTestClass):
             test_result['llstats'].append(curr_llstats)
             if current_ping_stats['connected']:
                 self.log.info(
-                    'Attenuation = {0}dB\tPacket Loss = {1}%\t'
-                    'Avg RTT = {2:.2f}ms\tRSSI = {3} [{4},{5}]\t'.format(
+                    'Attenuation = {0}dB\tPacket Loss = {1:.1f}%\t'
+                    'Avg RTT = {2:.2f}ms\tRSSI = {3:.2f} [{4},{5}]\t'.format(
                         atten, current_ping_stats['packet_loss_percentage'],
                         statistics.mean(current_ping_stats['rtt']),
                         current_rssi['signal_poll_rssi']['mean'],
@@ -397,6 +397,10 @@ class WifiPingTest(base_test.BaseTestClass):
                                     testcase_params['test_network']['SSID']):
             self.log.info('Already connected to desired network')
         else:
+            wutils.wifi_toggle_state(self.dut, False)
+            wutils.set_wifi_country_code(self.dut,
+                                         self.testclass_params['country_code'])
+            wutils.wifi_toggle_state(self.dut, True)
             wutils.reset_wifi(self.dut)
             wutils.set_wifi_country_code(self.dut,
                                          self.testclass_params['country_code'])
@@ -430,7 +434,7 @@ class WifiPingTest(base_test.BaseTestClass):
         self.setup_ap(testcase_params)
         # Set attenuator to 0 dB
         for attenuator in self.attenuators:
-            attenuator.set_atten(0, strict=False)
+            attenuator.set_atten(0, strict=False, retry=True)
         # Reset, configure, and connect DUT
         self.setup_dut(testcase_params)
 
