06a6860a2446f473e0365bdeb68808ab866ace71
tuan huynh <hmtuan@google.com>
2023-03-28 22:33:42 +0000

    Fix trailing 0 bandwidth in log.
    Bug: 272379332
    
    Change-Id: Ifbfae10d50e4eadbad11ad07331741b73d14f0b4

--- acts_tests/tests/google/power/tel/PowerTelTraffic_Preset_Test.py
+++ acts_tests/tests/google/power/tel/PowerTelTraffic_Preset_Test.py
@@ -14,8 +14,8 @@
 import os
 import time
 
+from acts import asserts
 import acts_contrib.test_utils.power.cellular.cellular_power_preset_base_test as PB
-
 
 class PowerTelTrafficPresetTest(PB.PowerCellularPresetLabBaseTest):
     # command to enable mobile data
@@ -63,6 +63,12 @@ class PowerTelTrafficPresetTest(PB.PowerCellularPresetLabBaseTest):
         # Call parent method first to setup simulation
         super().setup_test()
 
+        # get tput configs
+        self.unpack_userparams(abnormal_bandwidth_tolerance=0.1,
+                               bandwidth_tolerance=0.1)
+        self.expected_downlink_bandwidth = float(self.test_configs[self.test_name]['tput']['downlink'].split()[0])
+        self.expected_uplink_bandwidth = float(self.test_configs[self.test_name]['tput']['uplink'].split()[0])
+
         # setup ssh client
         self.ssh_iperf_client = self.cellular_simulator.create_ssh_client()
         self.ssh_iperf_server = self.cellular_simulator.create_ssh_client()
@@ -73,21 +79,77 @@ class PowerTelTrafficPresetTest(PB.PowerCellularPresetLabBaseTest):
         """Measure power while data is transferring."""
         # Start data traffic
         self.start_uplink_process()
-        time.sleep(5)
         self.start_downlink_process()
 
         # Measure power and check against threshold
         self.collect_power_data_and_validate()
 
-    def teardown_test(self):
-        super().teardown_test()
+    def _end_iperfs(self):
+        err_message = []
         # Write iperf log
         self.ssh_iperf_server.close()
         uplink_log_name = self.test_name + '_uplink.txt'
-        self._write_iperf_log(uplink_log_name, self.ssh_iperf_server)
+        out, err = self.iperf_out_err[self.ssh_iperf_server]
+        output_content = ''.join(out.readlines())
+        err_content = ''.join(err.readlines())
+        self._write_iperf_log(uplink_log_name, output_content + err_content)
+        if err_content.strip():
+            err_message.append(f'Uplink process fail due to error: {err_content}\n')
+        else:
+            if not self._iperf_log_check(output_content, self.expected_uplink_bandwidth):
+                err_message.append('Bandwidth of uplink process is unstable.')
+
         self.ssh_iperf_client.close()
         downlink_log_name = self.test_name + '_downlink.txt'
-        self._write_iperf_log(downlink_log_name, self.ssh_iperf_client)
+        out, err = self.iperf_out_err[self.ssh_iperf_client]
+        output_content = ''.join(out.readlines())
+        err_content = ''.join(err.readlines())
+        self._write_iperf_log(downlink_log_name, output_content + err_content)
+        if err_content.strip():
+            err_message.append(f'Downlink process fail due to error: {err_content}\n')
+        else:
+            if not self._iperf_log_check(output_content, self.expected_downlink_bandwidth):
+                err_message.append('Bandwidth of downlink process is unstable.')
+
+        if err_message:
+            raise RuntimeError('\n'.join(err_message))
+
+    def teardown_test(self):
+        self._end_iperfs()
+        super().teardown_test()
+
+    def _iperf_log_check(self, file, expected_bandwidth):
+        """Check iperf log and abnormal bandwidth instances.
+
+        Args:
+            file: file object of iperf log to be checked.
+            expected_bandwidth: integer value for expected bandwidth.
+        Returns:
+            True if log is normal, False otherwise.
+        """
+        # example of record line
+        #[  4]   0.00-1.00   sec  20.2 MBytes   169 Mbits/sec
+        total_abnormal_entries = 0
+        total_record_entries = 0
+        bandwidth_val_idx = 6
+        record_entry_total_cols = 8
+        lines = file.split('\n')
+        acceptable_difference = self.bandwidth_tolerance * expected_bandwidth
+        self.log.debug('Expected bandwidth: %f', expected_bandwidth)
+        self.log.debug('Acceptance difference: %f', acceptable_difference)
+        for line in lines:
+            cols = line.split()
+            self.log.debug(cols)
+            if len(cols) == record_entry_total_cols:
+                total_record_entries += 1
+                bandwidth = float(cols[bandwidth_val_idx])
+                self.log.debug('bandwidth: %f', bandwidth)
+                if abs(bandwidth-expected_bandwidth) > acceptable_difference:
+                    total_abnormal_entries += 1
+        if not total_record_entries:
+            raise RuntimeError('No tput data record found.')
+        self.log.debug('Total abnormal entries: %d - Total record: %d', total_abnormal_entries, total_record_entries)
+        return (total_abnormal_entries/total_record_entries) <= self.abnormal_bandwidth_tolerance
 
     def _exec_ssh_cmd(self, ssh_client, cmd):
         """Execute command on given ssh client.
@@ -142,23 +204,18 @@ class PowerTelTrafficPresetTest(PB.PowerCellularPresetLabBaseTest):
         self.log.info('UE iperf client started')
         time.sleep(5)
 
-    def _write_iperf_log(self, file_name, ssh):
+    def _write_iperf_log(self, file_name, content):
         """ Writing ssh stdout and stdin to log file.
 
         Args:
-            file_name: log file name to write log to.
-            ssh: paramiko client object.
+            file_name: Log file name to write log to.
+            content: Content to write to file.
         """
         iperf_log_dir = os.path.join(self.root_output_path, 'iperf')
         os.makedirs(iperf_log_dir, exist_ok=True)
         iperf_log_file_path = os.path.join(iperf_log_dir, file_name)
         with open(iperf_log_file_path, 'w') as f:
-            out, err = self.iperf_out_err[ssh]
-            out_content = ''.join(out.readlines())
-            err_content = ''.join(err.readlines())
-            f.write(out_content)
-            f.write('\nErrors:\n')
-            f.write(err_content)
+            f.write(content)
 
     def turn_on_mobile_data(self):
         self.dut.adb.shell(self.ADB_CMD_ENABLE_MOBILE_DATA)
